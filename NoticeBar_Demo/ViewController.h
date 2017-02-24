9e7d2ed9af78d412de1c573275fac70e74afcec5
阿郭 <821284957@qq.com>
2017-02-24 13:27:13 +0800

    Initial Commit

---
+++ NoticeBar_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NoticeBar_Demo
+//
+//  Created by yingcan on 17/2/24.
+//  Copyright © 2017年 guoxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NoticeBar_Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NoticeBar_Demo
+//
+//  Created by yingcan on 17/2/24.
+//  Copyright © 2017年 guoxuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
